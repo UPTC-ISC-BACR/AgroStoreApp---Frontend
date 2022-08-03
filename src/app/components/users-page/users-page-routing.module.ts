f760a6ae865834c736e0cd6ace5be5b35f853505
felipe <ssfeliperuizdd25@gmail.com>
2022-08-03 18:44:48 -0500

    AS-36 Vista de mi perfil integrada myprofile-user.component.ts, falta crear las funcionalidades de esta vista

--- src/app/components/users-page/users-page-routing.module.ts
+++ src/app/components/users-page/users-page-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {UsersPageComponent} from "./users-page.component";
 import {PrincipalPageUsersComponent} from "./principal-page-users/principal-page-users.component";
+import {MyprofileUserComponent} from "./myprofile-user/myprofile-user.component";
 
 const routes: Routes = [
   {path: '', component: UsersPageComponent, children: [
-      {path: '', component:PrincipalPageUsersComponent}
+      {path: '', redirectTo: 'principal', pathMatch: 'full'},
+      {path: 'principal', component:PrincipalPageUsersComponent},
+      {path: 'my-profile', component:MyprofileUserComponent}
     ]}
 ];
 
