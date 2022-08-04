9bd2d5787236b001f620e7d1d48cf8cd07d40dd1
felipe <ssfeliperuizdd25@gmail.com>
2022-08-04 10:34:08 -0500

    Angular Material, Cambiado para que se ajuste a la paleta de colores utilizada

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,6 +9,10 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatSelectModule} from "@angular/material/select";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatDialogModule} from "@angular/material/dialog";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatTableModule} from "@angular/material/table";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
 
 @NgModule({
   declarations: [],
@@ -22,7 +26,11 @@ import {MatDialogModule} from "@angular/material/dialog";
     MatInputModule,
     MatButtonModule,
     MatSelectModule,
-    MatDialogModule
+    MatDialogModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatIconModule,
+    MatMenuModule
   ],
   exports:[
     ReactiveFormsModule,
@@ -33,7 +41,11 @@ import {MatDialogModule} from "@angular/material/dialog";
     MatButtonModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatDialogModule
+    MatDialogModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatIconModule,
+    MatMenuModule
   ]
 })
 export class SharedModule { }
