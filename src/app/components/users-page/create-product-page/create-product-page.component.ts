b45ad55275832294c9f279e70354e08077f37704
Frankma0117 <franmacagu@gmail.com>
2022-08-05 19:55:20 -0500

    Agregar componente de nuevo producto

---
+++ src/app/components/users-page/create-product-page/create-product-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-product-page',
+  templateUrl: './create-product-page.component.html',
+  styleUrls: ['./create-product-page.component.css']
+})
+export class CreateProductPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
